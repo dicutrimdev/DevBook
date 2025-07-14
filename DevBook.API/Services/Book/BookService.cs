c6a59557c10c287eaad23611c7692d8241ab1167
dicutrimdev <diegocutrimcontato@hotmail.com>
2025-07-13 22:26:34 -0300

    Criação da interface IBookService e de sua implementação

---
+++ DevBook.API/Services/Book/BookService.cs
@@ -0,0 +1,46 @@
+﻿using DevBook.API.Data;
+using DevBook.API.Repositories.Book;
+
+namespace DevBook.API.Services.Book;
+
+public class BookService : IBookService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IBookRepository _bookRepository;
+
+    public BookService(
+        IUnitOfWork unitOfWork,
+        IBookRepository bookRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _bookRepository = bookRepository;
+    }
+
+    public async Task CreateAsync(Entities.Book book)
+    {
+        await _bookRepository.AddAsync(book);
+        await _unitOfWork.Commit();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _bookRepository.DeleteAsync(id);
+        await _unitOfWork.Commit();
+    }
+
+    public async Task<List<Entities.Book>> GetAllAsync()
+    {
+        return (await _bookRepository.GetAllAsync()).ToList();
+    }
+
+    public async Task<Entities.Book?> GetByIdAsync(Guid id)
+    {
+        return await _bookRepository.GetByIdAsync(id);
+    }
+
+    public async Task UpdateAsync(Entities.Book book)
+    {
+        await _bookRepository.Update(book);
+        await _unitOfWork.Commit();
+    }
+}
---
+++ DevBook.API/Services/Book/IBookService.cs
@@ -0,0 +1,10 @@
+﻿namespace DevBook.API.Services.Book;
+
+public interface IBookService
+{
+    public Task<List<Entities.Book>> GetAllAsync();
+    public Task<Entities.Book?> GetByIdAsync(Guid id);
+    public Task CreateAsync(Entities.Book author);
+    public Task UpdateAsync(Entities.Book author);
+    public Task DeleteAsync(Guid id);
+}
